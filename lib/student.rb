84357311d64665d461367db530d9e0b778d05332
Jason Mei <jason.mei403@gmail.com>
2018-11-08 19:20:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,6 @@ class Student
     UPDATE students SET name = ?, grade = ? WHERE id = ?;
     SQL
     
-    DB[:conn].execute(sql, self.name, self.album, self.id)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 end
